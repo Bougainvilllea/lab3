c236eed8b9805b31d056b6aa15574abc81022041
Bougainvilllea <80826067+Bougainvilllea@users.noreply.github.com>
2024-10-16 10:39:34 +0700

    Update PrimesGenerator.java

--- src/main/java/PrimesGenerator.java
+++ src/main/java/PrimesGenerator.java
@@ -1,3 +1,5 @@
+import java.math.BigInteger;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -6,20 +8,19 @@ public class PrimesGenerator {
     public List<Integer> list = new ArrayList<Integer>();
     public PrimesGenerator(Integer n){
 
-
         int i = 2;
         while(list.size() <= n) {
-
-            if ((i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0) || i == 2 || i == 3 || i == 5 || i == 7)  {
+            int integer = i;
+            BigInteger bigInteger = BigInteger.valueOf(integer);
+            boolean probablePrime = bigInteger.isProbablePrime((int) Math.log(integer));
+            System.out.println(probablePrime);
+            if (integer == 1){
                 list.add(i);
             }
             i++;
-
+            }
 
         }
 
 
     }
-}
-
-
